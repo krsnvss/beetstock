f94e186df6ac7e508fb75ed2b251e925b670be78
krsnvs <krsnvss@gmail.com>
2017-12-15 23:05:18 +0300

    Перенос из старого репозитория

---
+++ operators_gui/misc/checkers.py
@@ -0,0 +1,18 @@
+# Функции проверки значений введенных пользователем
+from re import match
+
+
+def name_checker(name):
+    correct_regexp = '([А-ЯЁ][а-яё]+[\-\s]?){2,}'
+    name_elements = []
+    for word in name.split(' '):
+        if word != ' ' and type(word) is str:
+            name_elements.append(word)
+    correct_name = str()
+    for element in name_elements:
+        correct_name += element.capitalize()
+        correct_name += ' '
+    if match(correct_regexp, correct_name) is not None:
+        return correct_name.strip()
+    else:
+        return None
---
+++ operators_gui/misc/labels.py
@@ -0,0 +1,15 @@
+# Разные текстовые заголовки, чтобы не хламить main.py
+
+# Форма редактирования рейсов
+# Заголовок
+edit_header = "Редактирование рейса №{}"
+# Сведения о карте
+info_label = '''
+<html>
+<head/>
+<body>
+<p>Автомобиль: <span style=" font-weight:600;">{}</span></p>
+<p>Прицеп: <span style=" font-weight:600;">{}</span></p>
+<p>Поставщик: <span style=" font-weight:600;">{}</span></p>
+</body>
+</html>'''
